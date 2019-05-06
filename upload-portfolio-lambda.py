64f75edd3f79e1b166d5d9f75396f53dff1d17a2
Rae Konn <raekon23@gmail.com>
2019-05-06 13:44:58 -0400

    Add script for lambda

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,18 @@
+import boto3
+from botocore.client import Config
+import StringIO
+import zipfile
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+portfolio_bucket = s3.Bucket('portfolio.raekonn.com')
+build_bucket = s3.Bucket('portfoliobuild.raekonn.com')
+
+portfolio_zip = StringIO.StringIO()
+build_bucket.download_fileobj('portfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+	for nm in myzip.namelist():
+		obj = myzip.open(nm)
+		portfolio_bucket.upload_fileobj(obj, nm)
+		portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
