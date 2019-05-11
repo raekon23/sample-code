3d004452be14cda058e83dd164a8cc6066038a15
Rae Konn <raekon23@gmail.com>
2019-05-11 09:37:55 -0400

    Updated lambda function

--- files/upload-portfolio-lambda.py
+++ files/upload-portfolio-lambda.py
@@ -12,11 +12,10 @@ def lambda_handler(event, context):
     }
     try:
         job = event.get("CodePipeline.job")
-        print "Jobs are: ", job
         if job:
             for artifact in job["data"]["inputArtifacts"]:
-                print "Artifacts are: ", artifact
-                if artifact["name"] == "BuildArtif":
+                print "Treating Artifact : ", artifact," with name ", artifact["name"]
+                if artifact["name"] == "BuildArtifact":
                     location = artifact["location"]["s3Location"]
                     print "BuildArtif successful"
         else:
